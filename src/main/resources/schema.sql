ec93381c3344a2a2833cb01a122aca16375bf836
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-04 16:52:46 -0300

    DATABASE CONNECTED AND WORKING!

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS purchase (
-   id INT AUTO_INCREMENT PRIMARY KEY,
+   id SERIAL PRIMARY KEY,
    product varchar(50) NOT NULL,
-   price double NOT NULL
+   price numeric NOT NULL
 );
