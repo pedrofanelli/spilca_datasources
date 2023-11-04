fff8e0486845f2bcd845a39df236cdce008dcc26
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-04 17:27:34 -0300

    building hikari

--- src/main/java/com/example/demo/SpilcaDatasourcesApplication.java
+++ src/main/java/com/example/demo/SpilcaDatasourcesApplication.java
@@ -1,15 +1,29 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"controllers","repositories"})
 public class SpilcaDatasourcesApplication {
 
+	@Value("${custom.datasource.url}")
+	private String datasourceUrl;
+	
+	@Bean
+	public DataSource dataSource() {
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpilcaDatasourcesApplication.class, args);
 	}
+	
+	
 
 }
